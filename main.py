75f6d8c5582fa2e9c923db95d25bbd93b9323495
649 <649@users.noreply.github.com>
2024-05-04 12:12:31 -0700

    init

---
+++ main.py
@@ -0,0 +1,30 @@
+'''
+LINGUANA v1.0
+Linguistics Analyzer
+Author: @037
+
+Application designed to take apart bodies of text and do comparative linguistics analysis.
+Purpose is to draw conclusions if two bodies of text share authorship.
+'''
+
+def n_gram_analyzer():
+    '''
+    Function is designed to take in an integer factorial to count from
+    Enumerates N-gram word pairings in a body of text
+    '''
+    print("[*] Starting N-gram Analyzer")
+
+def spelling_analyzer():
+    '''
+    Function that takes in a body of text
+    returns a list of spelling mistakes
+    '''
+    print("[*] Starting Spelling Analyzer")
+
+def word_frequency_analyzer():
+    '''
+    Function that takes in a body of text
+    returns every word used and its frequency
+    '''
+    print("[*] Starting Word Frequency Analyzer")
+
