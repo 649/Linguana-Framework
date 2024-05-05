253e398a942a072c281ba09aa42c5f90b1db61a7
649 <649@users.noreply.github.com>
2024-05-04 17:51:46 -0700

    v1.2 - added wpl and cpw logic

--- README.md
+++ README.md
@@ -1,13 +1,31 @@
 # LINGUANA
-## Linguistics Analyzer
+## Linguistics Analyser
 * Application designed for analyzing various forms of text
 * Builds fingerprints for classified text to associate authorship
 * Can do comparitive analysis on text, compare and contrast if two structures align
+
+## Setup
+1. Install dependencies
+```
+pip install -r requirements.txt
+```
+
+## Usage
+1. Fingerprint style of document
+```
+python3 ./main.py --input <PATH TO TEXT FILE>
+```
+2. Fingerprint style of document and output results
+```
+python3 ./main.py --input <PATH> --output <PATH>
+```
 
 ## Features
 * N-gram factorial style pairing
 * Spelling mistake tracking
 * Word frequency and usage
+* Words per line hits tracking
+* Characters per word hits tracking
 
 ## Future Plans
 * Web UI for tracking and uploading files
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 '''
-LINGUANA v1.1
+LINGUANA v1.2
 Linguistics Analyser
 Author: @037
 
@@ -13,6 +13,107 @@ from itertools import islice
 from autocorrect import Speller
 
 DEBUG = 0
+TOP_LIST = 10
+N_TERMS = 3
+STYLE_FINGERPRINT = []
+LOGO = '''
+                                                   __
+                                              _.-~`  `~-.
+                  _.--~~~---,.__          _.,;; .   -=(@'`\\
+               .-`              ``~~~~--~~` ';;;       ____)
+            _.'            '.              ';;;;;    '`_.'
+         .-~;`               `\           ' ';;;;;__.~`
+       .' .'          `'.     |           /  /;''
+        \\/      .---\'''``)   /'-._____.--'\  \\
+  jgs  _/|    (`        /  /`              `\ \__
+',    `/- \   \      __/  (_                /-\-\-`
+  `;'-..___)   |     `/-\-\-`
+    `-.       .'
+       `~~~~``
+ ██▓     ██▓ ███▄    █   ▄████  █    ██  ▄▄▄       ███▄    █  ▄▄▄      
+▓██▒    ▓██▒ ██ ▀█   █  ██▒ ▀█▒ ██  ▓██▒▒████▄     ██ ▀█   █ ▒████▄    
+▒██░    ▒██▒▓██  ▀█ ██▒▒██░▄▄▄░▓██  ▒██░▒██  ▀█▄  ▓██  ▀█ ██▒▒██  ▀█▄  
+▒██░    ░██░▓██▒  ▐▌██▒░▓█  ██▓▓▓█  ░██░░██▄▄▄▄██ ▓██▒  ▐▌██▒░██▄▄▄▄██ 
+░██████▒░██░▒██░   ▓██░░▒▓███▀▒▒▒█████▓  ▓█   ▓██▒▒██░   ▓██░ ▓█   ▓██▒
+░ ▒░▓  ░░▓  ░ ▒░   ▒ ▒  ░▒   ▒ ░▒▓▒ ▒ ▒  ▒▒   ▓▒█░░ ▒░   ▒ ▒  ▒▒   ▓▒█░
+░ ░ ▒  ░ ▒ ░░ ░░   ░ ▒░  ░   ░ ░░▒░ ░ ░   ▒   ▒▒ ░░ ░░   ░ ▒░  ▒   ▒▒ ░
+  ░ ░    ▒ ░   ░   ░ ░ ░ ░   ░  ░░░ ░ ░   ░   ▒      ░   ░ ░   ░   ▒   
+    ░  ░ ░           ░       ░    ░           ░  ░         ░       ░  ░
+                            Author: @037
+
+'''
+
+def wpl_analyser(body, top=10):
+    '''
+    Function is designed to record number of words per line
+    based on number of hits
+    '''
+    
+    wpl = {}
+    for lines in body:
+        word_count = len(lines)
+        try:
+            if(wpl[word_count] > 0):
+                wpl[word_count] = wpl[word_count] + 1
+        except KeyError:
+            wpl[word_count] = 1
+    # Sorting using a for loop
+    sorted_dict = {}
+    for key in sorted(wpl, key=wpl.get, reverse=True):
+        sorted_dict[key] = wpl[key]
+    print(f"[*] Top {top} words per line hits in body:")
+    i = 0
+    conjoined_string = ''
+    for word in sorted_dict:
+        print(f"[*] {word}: {sorted_dict[word]}")
+        conjoined_string += str(word) + ":"
+        # We take the N-th rank word count hits
+        if i == top:
+            break
+        else:
+            i += 1
+    conjoined_checksum = hashlib.sha256(conjoined_string.encode()).hexdigest()
+    STYLE_FINGERPRINT.append("W_" + conjoined_checksum)
+    print(f"[*] Top {top} WPL fingerprint: {conjoined_checksum}")
+    print()
+
+
+def cpw_analyser(body, top=10):
+    '''
+    Function is designed to record number of characters per word
+    based on number of hits
+    '''
+    
+    cpw = {}
+    for lines in body:
+        for word in lines:
+            word_length = len(word)
+            try:
+                if(cpw[word] > 0):
+                    cpw[word_length] = cpw[word_length] + 1
+            except KeyError:
+                cpw[word_length] = 1
+    # Sorting using a for loop
+    sorted_dict = {}
+    for key in sorted(cpw, key=cpw.get, reverse=True):
+        sorted_dict[key] = cpw[key]
+    print(f"[*] Top {top} char per word hits in body:")
+    i = 0
+    conjoined_string = ''
+    for word in sorted_dict:
+        print(f"[*] {word}: {sorted_dict[word]}")
+        conjoined_string += str(word) + ":"
+        # We take the N-th rank word frequent
+        if i == top:
+            break
+        else:
+            i += 1
+    conjoined_checksum = hashlib.sha256(conjoined_string.encode()).hexdigest()
+    STYLE_FINGERPRINT.append("C_" + conjoined_checksum)
+    print(f"[*] Top {top} CPW fingerprint: {conjoined_checksum}")
+    print()
+
+
 
 def n_gram_analyser(body, n=3, top=10):
     '''
@@ -23,9 +124,7 @@ def n_gram_analyser(body, n=3, top=10):
         shifted_list = input_list[shift:] + input_list[:shift]
         return [shifted_list[i:i+n] for i in range(0, len(input_list), n)]
     
-    ngram = {}
     for i in range(n, 1, -1):
-        print()
         print(f"[*] Taking {i}-gram of processed word list...")
         word_frequency = {}
         for j in range(0, i):
@@ -56,6 +155,7 @@ def n_gram_analyser(body, n=3, top=10):
             else:
                 x += 1
         conjoined_checksum = hashlib.sha256(conjoined_string.encode()).hexdigest()
+        STYLE_FINGERPRINT.append("N_" + conjoined_checksum)
         print(f"[*] Top {top} {i}-gram factorial words fingerprint: {conjoined_checksum}")
         print()
 
@@ -91,6 +191,7 @@ def spelling_analyser(body, top=10):
         else:
             i += 1
     conjoined_checksum = hashlib.sha256(conjoined_string.encode()).hexdigest()
+    STYLE_FINGERPRINT.append("S_" + conjoined_checksum)
     print(f"[*] Top {top} misspelled words fingerprint: {conjoined_checksum}")
     print()
 
@@ -124,6 +225,7 @@ def word_frequency_analyser(body, top=10):
         else:
             i += 1
     conjoined_checksum = hashlib.sha256(conjoined_string.encode()).hexdigest()
+    STYLE_FINGERPRINT.append("F_" + conjoined_checksum)
     print(f"[*] Top {top} used words fingerprint: {conjoined_checksum}")
     print()
 
@@ -132,13 +234,14 @@ if __name__ == "__main__":
     '''
     Main function for running multi threads for each analyser
     '''
-    print("[*] LINGUANA v1.1")
-    print("[*] Linguistics Analyser")
+    print(LOGO)
+    print("[*] LINGUANA v1.2 - Linguistics Analyser")
     spell = Speller(lang='en')
     if(len(sys.argv) > 2 and sys.argv[1] == '--input'):
         fn = sys.argv[2]
         if os.path.exists(fn):
-            print(os.path.basename(fn))
+            if(DEBUG == 1):
+                print(os.path.basename(fn))
             # file exists
             with open(fn, "rb") as target:
                 # Using this area to show a preview of the file being taken apart
@@ -176,12 +279,30 @@ if __name__ == "__main__":
                     for word in parsed_words:
                         print(word)
                 print()
+                print("------[BEGIN WPL HITS ANALYSER]------")
+                wpl_analyser(parsed_words, top=10)
+                print("------[BEGIN CPW HITS ANALYSER]------")
+                cpw_analyser(parsed_words, top=10)
                 print("------[BEGIN WORD FREQUENCY ANALYSER]------")
-                word_frequency_analyser(parsed_words)
+                word_frequency_analyser(parsed_words, TOP_LIST)
                 print("------[BEGIN N-GRAM ANALYSER]------")
-                n_gram_analyser(parsed_words)
+                n_gram_analyser(parsed_words, N_TERMS, TOP_LIST)
                 print("------[BEGIN SPELLING ANALYSER]------")
-                spelling_analyser(parsed_words)
-
+                spelling_analyser(parsed_words, TOP_LIST)
+                print("------[BEGIN LINGUANA FINGERPRINT]------")
+                STYLE_FINGERPRINT.sort()
+                for fp in STYLE_FINGERPRINT:
+                    print(fp)
+                print("------[END LINGUANA FINGERPRINT]------")
+                if(len(sys.argv) > 4 and sys.argv[3] == '--output'):
+                    fo = sys.argv[4]
+                    with open(fo, 'w') as file_output:
+                        # write variables using repr() function
+                        file_output.write("------[BEGIN LINGUANA FINGERPRINT]------" + '\n')
+                        STYLE_FINGERPRINT.sort()
+                        for fp in STYLE_FINGERPRINT:
+                            file_output.write(fp + '\n')
+                        file_output.write("------[END LINGUANA FINGERPRINT]------" + '\n')
     else:
         print(f"[*] Scan file: {sys.argv[0]} --input PATH")
+        print(f"[*] Save results: {sys.argv[0]} --input PATH --output PATH")
