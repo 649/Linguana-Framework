25671f36e07f246c92bc27caf4b2e266d7d3100d
649 <649@users.noreply.github.com>
2024-05-05 01:27:42 -0700

    v1.1 - added diff check feature

--- server.py
+++ server.py
@@ -17,15 +17,16 @@ from autocorrect import Speller
 from _thread import start_new_thread
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'supersecret'
-
+main.DATABASE = {}
+VERSION = '1.1'
 class DataForm(FlaskForm):
     data_input = TextAreaField('Document Content:')
     input_field = StringField('Profile Name:')
     delim_field = StringField('Delimiters:')
     submit = SubmitField('Analyse')
     version = '1.4'
+    server = VERSION
     logo = main.LOGO
     top_list = main.TOP_LIST
     n_terms = main.N_TERMS
@@ -65,6 +66,11 @@ def single_request(delimiter_list, data):
     response += "------[END LINGUANA FINGERPRINT]------" + '\n'
     return response
 
+@app.route('/saved')
+def saved():
+    data = list(main.DATABASE.items())
+    return render_template('diff.html', data=data)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = DataForm()
@@ -75,7 +81,7 @@ def index():
         delim = form.delim_field.data
         # Prints to console
         # print(data)
-
+        main.DATABASE[profile_name] = data
         return render_template('result.html', result=data, profile_name=profile_name, delim=delim)
     return render_template('index.html', form=form)
 
@@ -83,5 +89,5 @@ if __name__ == "__main__":
     '''
     Main function for running multi threads for each analyser
     '''
-    print("[*] LINGUANA WEB SERVER v1.0 - Linguistics Fingerprint Analyser")
+    print(f"[*] LINGUANA WEB SERVER v{VERSION} - Linguistics Fingerprint Analyser")
     app.run(debug=True)
